2acc12766ffb2915644b9c7dfe39f26f177f8589
przemek <host9@o2.pl>
2017-01-23 13:09:00 +0100

    Changed creating members map

---
+++ src/main/java/wydatki/WydatkiObject.java
@@ -0,0 +1,16 @@
+package wydatki;
+
+public class WydatkiObject {
+	    private Wydatki wydatki;
+
+	    public Wydatki getWydatki ()
+	    {
+	        return wydatki;
+	    }
+
+	    public void setWydatki (Wydatki wydatki)
+	    {
+	        this.wydatki = wydatki;
+	    }
+
+}
---
+++ src/main/java/wydatki/WydatkiPojo.java
@@ -0,0 +1,19 @@
+package wydatki;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WydatkiPojo {
+        @SerializedName("layers")
+        private WydatkiObject wydatkiobj;
+
+        public WydatkiObject getSpendings ()
+        {
+            return wydatkiobj;
+        }
+
+        public void setLayers (WydatkiObject layers)
+        {
+            this.wydatkiobj = layers;
+        }
+
+}
